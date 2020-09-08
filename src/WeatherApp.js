f668ee71d7585f03b1a177890ce8fb6d0ac306b1
Cátia Santos <64780751+supersoniccat@users.noreply.github.com>
2020-09-08 23:33:31 +0100

    Cleaned unused components & added Unit conversion

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -2,15 +2,31 @@ import React, { useState } from "react";
 import "./WeatherApp.css";
 import Axios from "axios";
 
-import Units from "./Units";
+
 import DetailsWeather from "./DetailsWeather";
 import Forecast from "./Forecast";
 
 export default function WeatherApp(props) {
   const [city, setCity] = useState(props.defaultCity);
   const [weatherData, setweatherData] = useState ({ready:false});
+  const [unit, setUnit] = useState("celsius");
+  
 
+  function showFahrenheit(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  }
+
+  function showCelsius(event) {
+    event.preventDefault();
+    setUnit("celsius");
+  }
+  
   function getWeather(response){
+    let celTemp = Math.round(response.data.main.temp);
+    let fahrTemp = (celTemp * 9) / 5 + 32;
+    
+    if (unit === "celsius") {
     setweatherData({
       ready: true,
       date: new Date (response.data.dt * 1000),
@@ -21,7 +37,19 @@ export default function WeatherApp(props) {
       details: response.data.weather[0].description,
       icon: response.data.weather[0].icon,      
     });
+  } else {
+    setweatherData({
+      ready: true,
+      date: new Date (response.data.dt * 1000),
+      city: response.data.name,
+      temperature: fahrTemp,
+      humidity: Math.round(response.data.main.humidity),
+      wind: Math.round(response.data.wind.speed),
+      details: response.data.weather[0].description,
+      icon: response.data.weather[0].icon,      
+    });
   }
+}
 
   function handleSubmit(event){
     event.preventDefault();
@@ -50,7 +78,22 @@ export default function WeatherApp(props) {
             </div>
           </div>
           <div className="col-3">
-            <Units />
+          <div className="Units">
+      <button className="weather-unit" id="celsius-btn" onClick={showCelsius}>
+        <img
+          src="\images\iconfinder_weather_21_2682830.png"
+          alt="celcius"
+          width="20px"
+        />
+      </button>
+      <button className="weather-unit" id="far-btn" onClick={showFahrenheit}>
+        <img
+          src="\images\iconfinder_weather_22_2682829.png"
+          alt="far"
+          width="20px"
+        />
+      </button>
+    </div>
           </div>
         </div>
         <DetailsWeather data={weatherData}/>
@@ -73,4 +116,4 @@ export default function WeatherApp(props) {
   Axios.get(apiUrl).then(getWeather);
   return "Loading...";
 }
-}
+  }
